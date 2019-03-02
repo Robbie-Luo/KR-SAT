f06981e0321c604d8555be878bba84a2d8b34d9d
Robbie <luobutang@gmail.com>
2019-03-02 23:07:07 +0100

    SAT-solver

--- SAT.py
+++ SAT.py
@@ -1,33 +1,63 @@
+#!/usr/bin/env python
+import time
+import numpy as np
 from utilities import *
 
+trackback_times=0
 class SATSolver:
-    def __init__(self, cnf):
+    def __init__(self, cnf, heuristic):
         self.cnf = cnf
-        self.satisfy = "normal"
+        self.heuristic = heuristic
         self.clauses = []
+        self.literals = []
         self.solution = []
+        self.time = 0
+        self.count = 0
 
     def initialize(self):
-        for rec in self.cnf:
-            rec = rec.strip()
-            if (rec[0].isdigit()) or (rec[0].startswith("-")):
-                clause=list(map(int,rec.split()[:-1]))
-                self.clauses.append(clause)
+        for line in self.cnf:
+            if line.startswith('p'):
+                continue
+            clause = [int(x) for x in line[:-2].split()]
+            for l in clause:
+                if abs(l) not in self.literals:
+                    self.literals.append(abs(l))
+            self.clauses.append(clause)
 
     def solve(self):
-        self.solution = solve_dfs(self.clauses)
+        start = time.time()
+        backtracking.count = 0
+        self.solution = backtracking(self.clauses, [], self.heuristic)
+        end = time.time()
+        # print("Heuristic: "+str(self.heuristic))
+        self.time = end - start
+
+        self.count = backtracking.count
+
+    def get_results(self):
+        S = [x for x in sorted(self.solution) if x > 0]
+        res = ''.join([(str(s)[-1]) for s in S])
+        return res, self.time, self.count
 
     def show_results(self):
-        print (self.solution)
+        print("Time: {:.4f}".format(self.time))
+        print("Count: {}".format(self.count))
+        if self.solution:
+            S= [x for x in sorted(self.solution) if x > 0]
+            print(np.array([int(str(s)[-1]) for s in S]).reshape(9, 9))
+        else:
+            print('UNSATISFIABLE')
 
 
 def main():
-    f = open("cnf-test.txt", "r")
+    heuristic, path = read_parameters()
+    f = open(path, "r")
     cnf = f.readlines()
-    solver = SATSolver(cnf)
+    solver = SATSolver(cnf, heuristic)
     solver.initialize()
     solver.solve()
     solver.show_results()
+
 
 if __name__ == "__main__":
     main()
--- utilities.py
+++ utilities.py
@@ -1,44 +1,196 @@
-# test if a predicate [p] holds for all elements of [l]
-def forall(l,p):
-  return len([x for x in l if p(x)]) == len(l)
+import random
+import sys
+def bcp(formula, unit):
+    modified = []
+    for clause in formula:
+        if unit in clause:
+            continue
+        if -unit in clause:
+            new_clause = [x for x in clause if x != -unit]
+            if not new_clause:
+                return -1
+            modified.append(new_clause)
+        else:
+            modified.append(clause)
+    return modified
 
-# test if a predicate [p] holds for some element of [l]
-def exists(l,p):
-  return len([x for x in l if p(x)]) > 0
 
-# flatten list of lists
-def flatten(l):
-  return [x for xs in l for x in xs]
-
-# remove duplicates from [l]
-# nb: does not guarantee order
-def uniquify(l):
-  return list(set(l))
+def get_counter(formula):
+    counter = {}
+    for clause in formula:
+        for literal in clause:
+            if literal in counter:
+                counter[literal] += 1
+            else:
+                counter[literal] = 1
+    return counter
 
 
-def symbols_of(f):
-  return uniquify([ abs(l) for l in flatten(f) ])
+def get_weighted_counter(formula, weight=2):
+    counter = {}
+    for clause in formula:
+        for literal in clause:
+            if literal in counter:
+                counter[literal] += weight ** -len(clause)
+            else:
+                counter[literal] = weight ** -len(clause)
+    return counter
 
-# c is satisfied by v
-# if c contains at least one literal which is true
-def clause_satisfied(c,v):
-  return exists(c, lambda l: l in v)
 
-# f is satisfied by v
-# if every clause of f is satisfied by v
-def satisfied(f,v):
-  return forall(f, lambda c: clause_satisfied(c,v))
+def get_weighted_abs_counter(formula, weight=2):
+    counter = {}
+    for clause in formula:
+        for literal in clause:
+            literal = abs(literal)
+            if literal in counter:
+                counter[literal] += weight ** -len(clause)
+            else:
+                counter[literal] = weight ** -len(clause)
+    return counter
 
-# f is unsatisfiable
-# if there exists a clause of f that is false (all literals are false)
-def unsatisfiable(f,v):
-  return exists(f, lambda c: forall(c, lambda l: -l in v))
 
-def solve_dfs(f):
-  def dfs(sym,v):
-    if satisfied(f,v): return v
-    elif unsatisfiable(f,v): return False
-    else:
-      l, s = sym[0], sym[1:]
-      return dfs(s, v+[l]) or dfs(s, v+[-l])
-  return dfs(symbols_of(f), [])
+def get_difference_counter(formula):
+    counter = {}
+    for clause in formula:
+        for literal in clause:
+            if literal in counter:
+                if literal > 0:
+                    counter[literal] += 1
+                else:
+                    counter[-literal] += - 1
+            else:
+                if literal > 0:
+                    counter[literal] = 1
+                else:
+                    counter[-literal] = - 1
+    return counter
+
+
+def pure_literal(formula):
+    counter = get_counter(formula)
+    assignment = []
+    pures = []
+    for literal, _ in counter.items():
+        if -literal not in counter:
+            pures.append(literal)
+    for pure in pures:
+        formula = bcp(formula, pure)
+    assignment += pures
+    return formula, assignment
+
+
+def unit_propagation(formula):
+    assignment = []
+    unit_clauses = [c for c in formula if len(c) == 1]
+    while unit_clauses:
+        unit = unit_clauses[0]
+        formula = bcp(formula, unit[0])
+        assignment += [unit[0]]
+        if formula == -1:
+            return -1, []
+        if not formula:
+            return formula, assignment
+        unit_clauses = [c for c in formula if len(c) == 1]
+    return formula, assignment
+
+
+def backtracking(formula, assignment, heuristic):
+    # print assignment
+    backtracking.count += 1
+    formula, pure_assignment = pure_literal(formula)
+    formula, unit_assignment = unit_propagation(formula)
+    assignment = assignment + unit_assignment  + pure_assignment
+    if formula == - 1:
+        return []
+    if not formula:
+        return assignment
+
+    variable = heuristic(formula)
+    solution = backtracking(bcp(formula, variable), assignment + [variable], heuristic)
+    if not solution:
+        solution = backtracking(bcp(formula, -variable), assignment + [-variable], heuristic)
+
+    return solution
+
+
+# Branching heuristics
+
+def heuristics_dict(heuristic):
+    heuristics = {
+        'JW'    : jeroslow_wang,
+        'RAN'   : random_selection,
+        'MO'    : most_often,
+        'SPC'   : shortest_positive_clause,
+        'FRE'   : freeman,
+        'JW2S'  : jeroslow_wang_2_sided
+    }
+    try:
+        return heuristics[heuristic]
+    except:
+        sys.exit("ERROR: '{}' Not valid heuristic.".format(heuristic) +
+                 "\nValid heuristics: {}".format(heuristics.keys()))
+
+
+def random_selection(formula):
+    counter = get_counter(formula)
+    return random.choice(list(counter.keys()))
+
+
+def jeroslow_wang(formula):
+    counter = get_weighted_counter(formula)
+    return max(counter, key=counter.get)
+
+def jeroslow_wang_2_sided(formula):
+    counter = get_weighted_abs_counter(formula)
+    return max(counter, key=counter.get)
+
+def most_often(formula):
+    counter = get_counter(formula)
+    return max(counter, key=counter.get)
+
+
+def shortest_positive_clause(formula):
+    min_len = sys.maxint
+    best_literal = 0
+    for clause in formula:
+        negatives = sum(1 for literal in clause if literal < 0)
+        if not negatives and len(clause) < min_len:
+            best_literal = clause[0]
+            min_len = len(clause)
+    if not best_literal:
+        return formula[0][0]
+    return best_literal
+
+def freeman(formula):
+    counter = get_difference_counter(formula)
+    max_p_literal = max(counter, key=counter.get)
+    max_n_literal = min(counter, key=counter.get)
+    if counter[max_p_literal] >= abs(counter[max_n_literal]):
+        return max_p_literal
+    return max_n_literal
+
+def max_key_value(counter):
+    keys = counter.keys()
+    values = counter.values()
+    return keys[values.index(max(values))]
+
+
+def read_parameters():
+    if len(sys.argv) != 3:
+        sys.exit("Use:python SAT.py <option> <cnf_file>")
+    heuristic = jeroslow_wang
+    option = sys.argv[1]
+    if "1" in option :
+        heuristic = random_selection
+        print("Heuristic:random_selection")
+    if "2" in option:
+        heuristic = jeroslow_wang
+        print("Heuristic:jeroslow_wang")
+    if "3" in option :
+        heuristic = most_often
+        print("Heuristic:most_often")
+    if "4" in option:
+        heuristic = jeroslow_wang_2_sided
+        print("Heuristic:jeroslow_wang_2_sided")
+    path=sys.argv[2]
+    return heuristic, path
